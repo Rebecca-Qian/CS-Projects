13081411ce38e917b87dce98fa5fe7d042dc83c6
Rebecca Qian <RebeccaQian@Rebeccas-MacBook-Pro-2.local>
2016-07-20 00:30:59 -0500

    javascript

--- E-Card-Generator/js/form.js
+++ E-Card-Generator/js/form.js
@@ -25,13 +25,39 @@ elClose.addEventListener('click', dismissNote, false);*/
 
 //document.body.removeChild(elNote);
 
-document.addEventListener("click", insertText);
-
-//document.getElementbyId('edit').addEventListener("click", insertText);
+document.getElementById("myCarousel").addEventListener("click", insertText);
 
 function insertText() {
 	document.getElementById("changefont").innerHTML = "Hello World";
 }
+
+// Removes the text in the form box
+
+var nameInput = document.getElementById("name");
+nameInput.addEventListener("click", clearText(nameInput));
+
+var emailInput = document.getElementById("email");
+emailInput = addEventListener("click", clearText(emailInput));
+document.getElementById("to").addEventListener("click", clearText);
+document.getElementById("message").addEventListener("click", clearText);
+
+function clearText(a) {
+	a.setAttribute('value', "");
+}
+
+/*function clearText(i) {
+	document.getElementById(controls[i]).setAttribute('value', "");
+};
+
+var controls = ["name", "email", "to", "message"]
+
+for (var i = 0; i < controls.length; i++) {
+	//function(removeText) {
+	var str = controls[i];
+	var nameInput = document.getElementById(str);
+	nameInput.addEventListener('click', clearText(i));
+//};
+};*/
 
 /*var textArea = document.getElementbyId("title");
 testArea.addEventListener("click", insertText, false);*/

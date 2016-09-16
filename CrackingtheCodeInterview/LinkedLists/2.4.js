de209c725e6889d9883484bd20020106aecf80d5
Rebecca Qian <RebeccaQian@Rebeccas-MacBook-Pro-2.local>
2016-09-16 01:20:27 -0700

    bleh

---
+++ CrackingtheCodeInterview/LinkedLists/2.4.js
@@ -0,0 +1,8 @@
+function sortList(node, x) {
+	var lessThan = new linkedList(0, null);
+	var greaterThan = new linkedList(0, null);
+	
+	if ((node.val < x) {
+		lessThan.val = node.val;
+		lessThan.next = 
+}
